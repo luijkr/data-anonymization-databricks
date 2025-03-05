3db280cdf56c819de87a9699afcfcf1df9e0347f
luijkr <luijk.r@gmail.com>
2025-03-05 19:43:14 +0100

    project creation

---
+++ src/data_anonymization/__init__.py
@@ -0,0 +1,1 @@
+"""Modules for the data_anonymization package."""
---
+++ src/data_anonymization/main.py
@@ -0,0 +1,11 @@
+"""Main functions for the data_anonymization package."""
+
+
+def main() -> bool:
+    """Entry point for the data_anonymization package."""
+    print("Hello, RevoData! \nWheel task is successfully completed.")
+    return True
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
